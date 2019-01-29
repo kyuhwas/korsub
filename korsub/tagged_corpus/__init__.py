218648a5504c5f20fc50faed39b978e6b9806a6a
lovit <soy.lovit@gmail.com>
2019-01-30 05:32:06 +0900

    Move to FourColumnCorpus (가제)

--- korsub/tagged_corpus/__init__.py
+++ korsub/tagged_corpus/__init__.py
@@ -1,3 +1,5 @@
+from .utils import FourColumnCorpus
+from .utils import decorate_four_column_to_lr_sent
 from .vectorizer import scan_subwords
 from .vectorizer import lr_sents_to_features
 from .vectorizer import scan_features
